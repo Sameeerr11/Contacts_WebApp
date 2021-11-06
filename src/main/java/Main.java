1141de7ddc30d6ebf57f069e68be301137a4cfb6
Sameeerr11 <Sameeerr11@gmail.com>
2021-11-06 15:40:28 -0500

    DAO class

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+import java.sql.SQLException;
+import java.util.List;
+import java.util.SortedMap;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        ContactsDAO cd = new ContactsDAO();
+        List<Contacts> l = cd.getContacts();
+
+        for(Contacts c: l)
+            System.out.println(" "+c.getFirstName());
+
+    }
+}
+*/
